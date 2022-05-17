3393f281bfce52ab0e7bf8bfaf0385581a04ef35
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2022-05-17 16:41:50 +0200

    server socket

--- Manager/inc/manager.h
+++ Manager/inc/manager.h
@@ -1,6 +1,32 @@
 #ifndef __MANAGER
 #define __MANAGER
 
+#define PORT 8080
+#define MAX_CONNECTIONS 8
+#define BUFFER_SIZE 1024
 
+// Different types of messages
+typedef enum
+{
+    m1,
+    m2,
+    m3
+} MessageType;
+
+// Different types of message senders
+typedef enum
+{
+    ELEVATOR,
+    FLOOR,
+    MANAGER
+} SenderType;
+
+// Struct for sending information packets between the clients and server
+struct dataPacket
+{
+    MessageType mType;
+    SenderType sType;
+    char mBuffer[BUFFER_SIZE];
+}
 
 #endif
